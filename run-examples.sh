f3a78b7c9a987ccb90173ed2da5e65be641d5288
Alex <alexbyrnes@gmail.com>
2015-11-15 22:56:13 -0500

    cleaner sample file

--- run-examples.sh
+++ run-examples.sh
@@ -3,8 +3,8 @@
 # bounding boxes found.
 ./datapiece -i pngs/contract2.png -b boxes_contract.json -o out/contract2_one_line.png --dpi 300 --jsonout out/contract2.json
 
-# Output series of images named out/contract2_<field>.png with --split.
-./datapiece -i pngs/contract2.png -b boxes_contract.json -o out/contract2_ --dpi 300 --split 
+# Output series of images named out/contract2_<field>.png with --split. Clean a 2 pixel border around each output image.
+./datapiece -i pngs/contract2.png -b boxes_contract.json -o out/contract2_ --dpi 300 --split -B 2
 
 # Output JSON without images with --findonly.
 ./datapiece -i pngs/contract2.png -b boxes_contract.json --dpi 300 --jsonout out/contract2_findonly.json --findonly
