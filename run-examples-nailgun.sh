4698c0afabc452a29f46da6b9c4009b9b3b2a877
Alex Byrnes <alexbyrnes@gmail.com>
2015-11-15 21:01:26 -0500

    Delete run-examples-nailgun.sh

--- run-examples-nailgun.sh
+++
@@ -1,8 +0,0 @@
-# Needs nailgun server. Run run-nailgun-server.sh
-
-
-# Crop three FCC contracts.  Output will be out/contract1_out.png etc.
-
-ng "com.datapiece.Datapiece" -i pngs/contract1.png -b boxes_contract.json -o out/contract1_cropped.png --dpi 300
-ng "com.datapiece.Datapiece" -i pngs/contract2.png -b boxes_contract.json -o out/contract2_cropped.png --dpi 300
-ng "com.datapiece.Datapiece" -i pngs/contract3.png -b boxes_contract.json -o out/contract3_cropped.png --dpi 300
